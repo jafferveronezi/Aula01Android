b571f1f1662de88333e1a40c684252694fbfecce
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-28 23:04:21 -0300

    alteraçoes na tela

--- app/src/main/java/br/com/jaffer/primeiroprojeto/FormularioActivity.kt
+++ app/src/main/java/br/com/jaffer/primeiroprojeto/FormularioActivity.kt
@@ -2,11 +2,21 @@ package br.com.jaffer.primeiroprojeto
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class FormularioActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario)
+
+        val peso = findViewById<EditText>(R.id.edit_peso)
+        val altura = findViewById<EditText>(R.id.edit_altura)
+        val botaoCalcular = findViewById<Button>(R.id.btn_calcular)
+        val botaoLimpar = findViewById<Button>(R.id.btn_limpar)
+
+
     }
+
 }
