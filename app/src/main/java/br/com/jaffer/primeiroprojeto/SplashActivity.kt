b2ae93c3501b539cd49e10509f8709263a8cd8c8
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-28 21:46:06 -0300

    tela de splash

--- app/src/main/java/br/com/jaffer/primeiroprojeto/SplashActivity.kt
+++ app/src/main/java/br/com/jaffer/primeiroprojeto/SplashActivity.kt
@@ -1,12 +1,28 @@
 package br.com.jaffer.primeiroprojeto
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        carregar()
     }
+
+    private fun carregar() {
+        Handler().postDelayed({chamarProximaTela()},2000L)
+    }
+
+    private fun chamarProximaTela() {
+        var proximaTelaIntent = Intent(this,
+                FormularioActivity::class.java)
+        startActivity(proximaTelaIntent)
+        finish()
+    }
+
+
 }
